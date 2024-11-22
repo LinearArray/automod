375a3bac9c49829cd911422610360029b4bf41cc
LinearArray <161222228+LinearArray@users.noreply.github.com>
2024-11-23 03:13:03 +0530

    Make subreddit name configurable via SUBREDDIT_NAME environment variable

--- update_automod.py
+++ update_automod.py
@@ -7,10 +7,10 @@ def update_automod_config():
         client_secret=os.environ['REDDIT_SECRET'],
         username=os.environ['REDDIT_USERNAME'],
         password=os.environ['REDDIT_PASSWORD'],
-        user_agent='test'
+        user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
     )
 
-    subreddit = reddit.subreddit('test') # replace with your subreddit name
+    subreddit = reddit.subreddit(os.environ['SUBREDDIT_NAME'])  
 
     with open('automod/automoderator.yml', 'r') as file:
         automod_config = file.read()
